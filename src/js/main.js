027cb132fcafce057fcf4c4d5db59953f8f32f10
Alex <bondalexander62@gmail.com>
2022-05-01 21:49:23 +0300

    pictureSize func

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ import showHiddenStyles from "./modules/showHiddenStyles";
 import showServerCards from "./modules/showServerCards";
 import calc from "./modules/calc";
 import filter from "./modules/filter";
+import pictureSize from "./modules/pictureSize";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -22,4 +23,5 @@ window.addEventListener('DOMContentLoaded', () => {
     showServerCards('.button-styles', '#styles .row');
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
     filter();
+    pictureSize('.sizes-block');
 });
